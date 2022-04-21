69421bff638e27f22adca6f036a8dea3dfa7742c
ARMUMTAAJ <armumtaaj@gmail.com>
2022-04-21 12:39:20 -0400

    My First commit and push to cloud Repo

---
+++ python/f2.py
@@ -0,0 +1,2 @@
+What is Vim editor used for?
+Vim is a highly configurable text editor built to enable efficient text editing. It is an improved version of the vi editor distributed with most UNIX systems. Vim is often called a "programmer's editor," and so useful for programming that many consider it an entire IDE . It's not just for programmers, though.
